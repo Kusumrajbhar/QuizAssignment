9eef5d4ec5b0f39588fdb0cef9b9443aa2cd029a
Kusum Rajbhar <2198401@tcs.com>
2022-06-29 17:08:26 +0530

    added score at final page

--- src/Component/ThankYouPage/ThankYouPage.js
+++ src/Component/ThankYouPage/ThankYouPage.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./ThankYou.scss";
 import "../Footer/Footer.scss";
+import { arrayContext } from '../../App';
 import Footer from '../Footer/Footer';
 
 function ThankYouPage() {
     const navigate = useNavigate();
+    const {scoreState} = useContext(arrayContext)
     const handleClick = () => {
       navigate("/");
     }
   return (
     <div className='main'>
         <h1 className='heading'>Thank You</h1>
+        <h3>Your Score is {scoreState} </h3>
         <div className='footer'>
             <button className='home-button' onClick={handleClick}>Home</button>
         </div>
